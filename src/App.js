836c241221f2c80c1e58938478476d8ab707a71d
Scribani <scribani.andres@gmail.com>
2021-10-22 15:34:13 -0500

    Make hot fixes to App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import "./App.css";
-import Title from "./components/Title";
+import { TitleH1 } from "./components/Title";
 import Error404 from "./pages/Error404";
 import Followers from "./pages/users/Followers";
+import Repos from "./pages/users/Repos";
 import Home from "./pages/Home";
 import Followings from "./pages/users/Followings";
 import styled from "@emotion/styled";
@@ -11,7 +12,7 @@ const Main = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
-`
+`;
 
 function App() {
   return (
@@ -20,14 +21,14 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/search">
-            <Title> page Search not found</Title>
+            <TitleH1> page Search not found</TitleH1>
           </Route>
           <Route path="/favorite">
-            <Title> page Favorites not found</Title>
+            <TitleH1> page Favorites not found</TitleH1>
           </Route>
           <Route path="/users/:username/followers" component={Followers} />
           <Route path="/users/:username/followings" component={Followings} />
-          <Route path="/users/:username/repos" component={Followings} />
+          <Route path="/users/:username/repos" component={Repos} />
           <Route path="*" component={Error404} />
         </Switch>
         <div className="footer">
@@ -37,7 +38,6 @@ function App() {
         </div>
       </Router>
     </Main>
-
   );
 }
 
