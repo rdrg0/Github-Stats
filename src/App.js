fc66fd28f674ecbf8ee299b78ec9d5a60b5cd671
Alexander-Valladares <ploxx95@gmail.com>
2021-10-21 22:08:09 -0500

    add app_component , add ROUTES to  pages home , error, and users

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Title from "./components/Title";
+import Error404 from "./pages/Error404";
+import Followers from "./pages/users/Followers";
+import Home from "./pages/Home";
+import Followings from "./pages/users/Followings";
 
 function App() {
   return (
-    <div className="">
-      <Title />
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/search">
+            <Title> page Search not found</Title>
+          </Route>
+          <Route path="/favorite">
+            <Title> page Favorites not found</Title>
+          </Route>
+          <Route path="/users/:username/followers" component={Followers} />
+          <Route path="/users/:username/followings" component={Followings} />
+          <Route path="/users/:username/repos" component={Followings} />
+          <Route path="*" component={Error404} />
+        </Switch>
+        <div className="footer">
+          <Link to="/">Home</Link>
+          <Link to="/search">Search</Link>
+          <Link to="/favorite">Favorite</Link>
+        </div>
+      </Router>
     </div>
   );
 }
