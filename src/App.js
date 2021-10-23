8e08381b4036a611b97a3e13f312423daec25168
Scribani <scribani.andres@gmail.com>
2021-10-23 06:15:25 -0500

    Make hot fixes

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
         <div className="footer">
           <Link to="/">Home</Link>
           <Link to="/search">Search</Link>
-          <Link to="/favorite">Favorite</Link>
+          <Link to="/favorites">Favorites</Link>
         </div>
       </Router>
     </Main>
